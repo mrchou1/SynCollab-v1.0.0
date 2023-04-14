870ebe0ebdbffdf84d2faec022f68aabd8f856db
mrchou1 <50980866+mrchou1@users.noreply.github.com>
2023-04-14 13:38:25 +0000

    supabase

--- supabase/index.tsx
+++ supabase/index.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://bcqincllvupgnvfhfldh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxOTg1NzcwOCwiZXhwIjoxOTM1NDMzNzA4fQ.YRYiFpLIvlP-e9R0gvDIoo3_cWA1qcjThLARVrfE_58"
+  "https://mrbbfkzzhbfuacwdzohf.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yYmJma3p6aGJmdWFjd2R6b2hmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE0NzkzNTUsImV4cCI6MTk5NzA1NTM1NX0.8Ate2Ug4q80xGilQsbLxmQEPcT_pAfS6n42DX_d0_Qk"
 );
